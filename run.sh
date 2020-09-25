c463dc61d8d32edbeb94624fa2cc73a2eb18f977
Bart Dorsey <bart@bartdorsey.com>
2020-09-25 17:18:02 -0500

    Adding curlable run script

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+if [ -d $HOME/.aa-setup-checker ]; then
+   cd ~/.aa-setup-checker
+   git pull --rebase
+   ~/.aa-setup-checker/check.sh
+   cd -
+   exit 0;
+fi
+
+mkdir $HOME/.aa-setup-checker
+git clone https://github.com/bartdorsey/aa-setup-checker.git ~/.aa-setup-checker
+
+. ~/.aa-setup-checker/check.sh
