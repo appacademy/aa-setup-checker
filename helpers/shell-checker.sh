3b3f43fbcc172435243b965a869d6c18b1e2b273
Bart Dorsey <bart@bartdorsey.com>
2020-09-30 15:55:56 -0500

    Changing shell script to run use title function

--- helpers/shell-checker.sh
+++ helpers/shell-checker.sh
@@ -3,7 +3,7 @@
 source ./helpers/colors.sh
 source ./helpers/utils.sh
 hr
-f_bold "Checking Shell"
+title "Checking Shell"
 hr
 c_green "Shell: $SHELL"
 c_green "Shell Startup File: $(shell_startup_file)"
