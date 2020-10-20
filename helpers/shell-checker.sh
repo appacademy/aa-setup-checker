e4f3bfc9a733733a097ac991a9d810d39489d564
Bart Dorsey <bart@bartdorsey.com>
2020-10-20 17:46:04 -0500

    Let's just run the shell startup file.

--- helpers/shell-checker.sh
+++ helpers/shell-checker.sh
@@ -5,3 +5,6 @@ title "Checking Shell"
 hr
 c_green "Shell: $SHELL"
 c_green "Shell Startup File: $(shell_startup_file)"
+c_green "Running shell startup file..."
+source $(shell_startup_file) > /dev/null 2>&1
+
