2866ab0fe18d6135455cf7cddf2f446a77993a66
Bart Dorsey <bart@bartdorsey.com>
2020-11-03 12:04:27 -0600

    Removing debug line

--- helpers/python-checker.sh
+++ helpers/python-checker.sh
@@ -39,7 +39,7 @@ if [ ! -d $HOME/.pyenv ]; then
     echo
     f_bold "curl https://pyenv.run | bash"
 fi
-echo $PYENV_IN_STARTUP_FILES
+
 if [ $PYENV_IN_STARTUP_FILES -eq 0 ]; then
     c_red "pyenv isn't in your startup files"
     c_red "Add these lines to your $STARTUP_FILE"
